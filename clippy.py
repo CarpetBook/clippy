905b2115123527d3146c552ae6ab4fe0b74dd94d
hako⭐ <forrestlightspeed@gmail.com>
2024-02-02 18:48:02 -0600

    refactor main event loop; use "if name == main"; support clipping audio

--- clippy.py
+++ clippy.py
@@ -129,18 +129,49 @@ layout = [
         sg.Frame(
             "Export settings",
             layout=[
+                # [
+                #     sg.Checkbox("Export as audio only", key="audio_only_check", enable_events=True),
+                # ],
                 [
-                    sg.Text("Resolution"),
+                    sg.Text("Resolution", key="reso_label"),
                     sg.Combo(
                         RES_OPTIONS,
                         default_value=defaults.get("resolution"),
                         key="resolution",
                         enable_events=True,
                     ),
+                    # # only visible when attaching image to audio
+                    # sg.Checkbox(
+                    #     "Turn audio into video",
+                    #     key="audio_to_video_check",
+                    #     visible=False,
+                    #     default=defaults.get("audio_to_video_check"),
+                    #     enable_events=True,
+                    #     tooltip=tooltip("audio_to_video_check"),
+                    # ),
                 ],  # default to 720p
                 [
-                    sg.Text("FPS"),
-                    sg.InputText(defaults.get("fps"), size=(5, 1), key="fps", enable_events=True),
+                    sg.Text("FPS", key="fps_label"),
+                    sg.InputText(
+                        defaults.get("fps"), size=(5, 1), key="fps", enable_events=True
+                    ),
+                    # only visible when audio_to_video_check is checked
+                    sg.Text("Audio output", key="container_label", visible=False),
+                    sg.Combo(
+                        AUD_CONTAINERS,
+                        default_value=defaults.get("export_container"),
+                        key="export_container",
+                        visible=False,
+                        enable_events=True,
+                    ),
+                    # # only visible when audio_to_video_check is checked
+                    # sg.Text("Image to attach", key="image_label", visible=False),
+                    # sg.InputText(
+                    #     key="image_to_attach",
+                    #     visible=False,
+                    #     size=(15, 1),
+                    # ),
+                    # sg.FileBrowse("Browse", key="browse_image", visible=False),
                 ],
             ],
         ),
@@ -227,6 +258,55 @@ def update_res_warning(window):
         window["res_warning"].update(builder)
 
 
+def toggle_export_options(window, audio_only):
+    """Toggle export options."""
+    # hide resolution and fps if audio
+    window["reso_label"].update(visible=not audio_only)
+    window["resolution"].update(visible=not audio_only)
+    window["fps_label"].update(visible=not audio_only)
+    window["fps"].update(visible=not audio_only)
+
+    # hide auto res button
+    window["auto_res"].update(visible=not audio_only)
+
+    # show audio containers and audio_to_video_check
+    window["container_label"].update(visible=audio_only)
+    window["export_container"].update(visible=audio_only)
+    # window["audio_to_video_check"].update(visible=audio_only)
+
+    # disable audio container if audio to video
+    # audio_to_video = window["audio_to_video_check"].get()
+    # window["export_container"].update(disabled=audio_to_video)
+
+
+def toggle_attach_image(audio_to_video):
+    """Toggle attach image."""
+    # hide audio container
+    window["container_label"].update(visible=not audio_to_video)
+    window["export_container"].update(visible=not audio_to_video)
+
+    # show image to attach options
+    window["image_label"].update(visible=audio_to_video)
+    window["image_to_attach"].update(visible=audio_to_video)
+    window["browse_image"].update(visible=audio_to_video)
+
+
+def update_extension_on_format(window, values):
+    """Update extension on format change."""
+    if values["output_file"].split(".")[-1] in AUD_CONTAINERS + ["mp4"]:
+        # replace extension
+        new_output_file = (  # this code is fugly
+            ".".join(values["output_file"].split(".")[:-1]) + f".{values['export_container']}"  # fmt: skip
+        )
+        window["output_file"].update(new_output_file)
+        return
+
+    if not values["output_file"].endswith(values["export_container"]):
+        window["output_file"].update(
+            values["output_file"] + "." + values["export_container"]
+        )
+        return
+
 def show_custom_error(message, title="Oops!"):
     sg.popup_error(message, title=title, icon=SCISSORS_ICON)
 
@@ -291,163 +371,194 @@ def check_for_fftools():
 
 just_clipped = None
 last_values = defaults
+input_audio_only_or_extract_audio = False
 
-check_for_fftools()
 
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    print(values)
-    if values is not None:
-        last_values = values
-    if event in (None, "cancel"):  # if user closes window or clicks cancel
-        save_settings(last_values)
-        break
-    elif event == "start":
-        if values["input_file"] == "":
-            show_custom_error("Please choose a file to clip using the 'Browse' button.")
-            continue
-        if values["clip_loc"] == "":
-            show_custom_error("Please choose a folder to save the clipped file.")
-            continue
-        if not os.path.isdir(values["clip_loc"]):
-            show_custom_error("Save folder does not exist.")
-            continue
-        if values["output_file"] == "":
-            show_custom_error("Please choose a file name for the clipped file.")
-            continue
-        if values["input_file"] == values["output_file"]:
-            show_custom_error("Input and output files cannot be the same.")
-            continue
-        if values["fps"] == "0":
-            show_custom_error("FPS must be more than 0.")
-            continue
-        if not values["fps"].isdigit():
-            show_custom_error("FPS must be a number.")
-            continue
-        if re.findall(SPECIAL_CHAR_CHECK, values["output_file"]):
-            show_custom_error("File name cannot contain special characters.")
-            continue
+def main_app():
+    global just_clipped, last_values, input_audio_only_or_extract_audio
+    # main window event loop
+    while True:
+        event, values = window.read()
+        print(values)
+        if values is not None:
+            last_values = values
 
-        was_just_clipped = just_clipped == values["output_file"]
-        already_exists = os.path.exists(
-            os.path.join(values["clip_loc"], values["output_file"])
-        )
-        if was_just_clipped or already_exists:
-            ans = show_custom_yesno(
-                "This file already exists in the save folder.\nAre you sure you want to overwrite the file?"
-            )
-            if ans == "No" or ans is None:
+        if event in (None, "cancel"):  # if user closes window or clicks cancel
+            save_settings(last_values)
+            break
+
+        elif event == "start":
+            _ok = start_data_validation(values)
+            if not _ok:
                 continue
 
-        # check if file name has .mp4 extension
-        # if not, add it
-        if not values["output_file"].endswith(".mp4"):
-            values["output_file"] += ".mp4"
-        # correct output file path with clip_loc
-        values["output_file"] = os.path.join(values["clip_loc"], values["output_file"])
-        # send values to construct ffmpeg command
-        cmd = fp.construct_from_sg_values(values)
-        # replace ffmpeg location
-        cmd[0] = ffmpeg_loc
-        # run ffmpeg command yielding progress
-        duration = values["end_time_slider"] - values["start_time_slider"]
-        window.perform_long_operation(
-            lambda: run_yielding_ffmpeg_exc(cmd, window, duration),
-            end_key="ffmpeg_done",
-        )
-        # break
-
-    # update slider ranges on input file change
-    if event == "input_file":
-        try:
-            probe = ffmpeg.probe(
-                values["input_file"],
-                cmd=ffprobe_loc,
-                popen_kwargs={"creationflags": CREATE_NO_WINDOW},
+            was_just_clipped = (
+                just_clipped == values["output_file"] + "." + values["export_container"]
             )
-        except ffmpeg.Error as e:
-            sg.popup_error(e.stderr)
-            continue
-        # window["input_file"].update(values["input_file"])
-        duration = probe["format"]["duration"]
-        duration = float(duration)
-        # assign resolution metadata to warning
-        frame_numer, frame_denom = probe["streams"][0]["r_frame_rate"].split("/")
-        frame_numer = int(frame_numer)
-        frame_denom = int(frame_denom)
-        official_fps = frame_numer / frame_denom
-        window["res_warning"].metadata = {
-            "reso_y": probe["streams"][0]["height"],
-            "fuhpis": official_fps,
-        }
-        window["start_time_slider"].update(range=(0, duration))
-        window["end_time_slider"].update(range=(0, duration))
-        window["end_time_slider"].update(value=duration)
-        window["end_time_input"].update(tc.get_time(duration))
-        # update clip length
-        update_clip_length_text(window)
+            already_exists = os.path.exists(
+                os.path.join(values["clip_loc"], values["output_file"])
+            )
+            if was_just_clipped or already_exists:
+                ans = show_custom_yesno(
+                    "This file already exists in the save folder.\nAre you sure you want to overwrite the file?"
+                )
+                if ans == "No" or ans is None:
+                    continue
+
+            # check if file name has extension
+            # if not, add it
+            _extension = (
+                values["export_container"]
+                if input_audio_only_or_extract_audio
+                else "mp4"
+            )
+            if not values["output_file"].endswith(_extension):
+                values["output_file"] += f".{_extension}"
+            # correct output file path with clip_loc
+            values["output_file"] = os.path.join(
+                values["clip_loc"], values["output_file"]
+            )
+            # send values to construct ffmpeg command
+            # add audio_only flag
+            values["audio_only"] = input_audio_only_or_extract_audio
+            cmd = fp.construct_from_sg_values(values)
+            # replace ffmpeg location
+            cmd[0] = ffmpeg_loc
+            # run ffmpeg command yielding progress
+            duration = values["end_time_slider"] - values["start_time_slider"]
+            window.perform_long_operation(
+                lambda: run_yielding_ffmpeg_exc(cmd, window, duration),
+                end_key="ffmpeg_done",
+            )
+            # break
+
+        # update slider ranges on input file change
+        if event == "input_file":
+            try:
+                probe = fp.get_file_info(values["input_file"], ffprobe_loc)
+                info = fp.parse_ffprobe_info(probe)
+                # store info in input file box
+                window["input_file"].metadata = info
+
+                # if audio only
+                window["resolution"].update(disabled=info["audio_only"])
+                input_audio_only_or_extract_audio = info["audio_only"]
+
+                window["res_warning"].metadata = {
+                    "reso_y": info["reso_y"],
+                    "fps": info["fps"],
+                }
+                duration = info["duration"]
+                window["start_time_slider"].update(range=(0, duration))
+                window["end_time_slider"].update(range=(0, duration))
+                window["end_time_slider"].update(value=duration)
+                window["end_time_input"].update(tc.get_time(duration))
+            except fp.NoStreamsFoundException as e:
+                _er_str = "Input file seems to be empty:\n" + str(e)
+                show_custom_error(_er_str)
+                window["input_file"].update("")
+                continue
+            except FfmpegError as e:
+                _er_str = "Input file seems to be broken:\n" + str(e.stderr, "utf-8")
+                show_custom_error(_er_str)
+                window["input_file"].update("")
+                continue
+            except Exception as e:
+                _er_str = "Error opening input file:\n" + str(e)
+                show_custom_error(_er_str)
+                window["input_file"].update("")
+                continue
+            # update clip length
+            update_clip_length_text(window)
+            # check resolution warning
+            update_res_warning(window)
+            # is audio only? if so, disable resolution
+            toggle_export_options(window, input_audio_only_or_extract_audio)
+            # set clip file name
+            filey = os.path.basename(values["input_file"])
+            filey = os.path.splitext(filey)[0]
+            container_ext = (
+                values["export_container"]
+                if input_audio_only_or_extract_audio
+                else "mp4"
+            )
+            window["output_file"].update(filey + f"_clippy.{container_ext}")
+
+        # checking controls
+        if event == "start_time_slider":
+            text = tc.get_time(values["start_time_slider"])
+            window["start_time_input"].update(text)
+            update_clip_length_text(window)
+        if event == "end_time_slider":
+            text = tc.get_time(values["end_time_slider"])
+            window["end_time_input"].update(text)
+            update_clip_length_text(window)
+
+        # check time input boxes
+        if event == "start_time_input":
+            # if error during conversion, pass until time is valid
+            # start process will check for valid time
+            sec = is_time_valid(values["start_time_input"])
+            if sec is False:
+                continue
+            window["start_time_slider"].update(sec)
+            update_clip_length_text(window)
+        if event == "end_time_input":
+            sec = is_time_valid(values["end_time_input"])
+            if sec is False:
+                continue
+            window["end_time_slider"].update(sec)
+            update_clip_length_text(window)
+
         # check resolution warning
-        update_res_warning(window)
-        # set clip file name
-        filey = os.path.basename(values["input_file"])
-        filey = os.path.splitext(filey)[0]
-        window["output_file"].update(filey + "_clippy.mp4")
+        if event == "resolution" or event == "fps":
+            if values["fps"] == "":
+                window["fps"].update("0")
+            update_res_warning(window)
 
-    # checking controls
-    if event == "start_time_slider":
-        text = tc.get_time(values["start_time_slider"])
-        window["start_time_input"].update(text)
-        update_clip_length_text(window)
-    if event == "end_time_slider":
-        text = tc.get_time(values["end_time_slider"])
-        window["end_time_input"].update(text)
-        update_clip_length_text(window)
+        # update extension on format change
+        if event == "export_container":
+            update_extension_on_format(window, values)
 
-    # check time input boxes
-    if event == "start_time_input":
-        # if error during conversion, pass until time is valid
-        try:
-            sec = tc.get_sec(values["start_time_input"])
-        except ValueError:
+        # if progress is made
+        if event == "ffmpeg_progress":
+            window["progressbar"].update(values["ffmpeg_progress"])
+            window["status"].update(get_status_text(values["ffmpeg_progress"]))
             continue
-        window["start_time_slider"].update(sec)
-        update_clip_length_text(window)
-    if event == "end_time_input":
-        try:
-            sec = tc.get_sec(values["end_time_input"])
-        except ValueError:
-            continue
-        window["end_time_slider"].update(sec)
-        update_clip_length_text(window)
 
-    # check resolution warning
-    if event == "resolution" or event == "fps":
-        if values["fps"] == "":
-            window["fps"].update("0")
-        update_res_warning(window)
+        # if ffmpeg is done
+        if event == "ffmpeg_done":
+            window["status"].update(get_status_text(0, True))
+            window["progressbar"].update(0)
+            window["start"].update(disabled=False)
+            if values["ffmpeg_done"] == "error":
+                continue
+            joined_path = os.path.join(values["clip_loc"], values["output_file"])
+            done_message(joined_path)
+            just_clipped = values["output_file"] + "." + values["export_container"]
+            # break
 
-    # if progress is made
-    if event == "ffmpeg_progress":
-        window["progressbar"].update(values["ffmpeg_progress"])
-        window["status"].update(get_status_text(values["ffmpeg_progress"]))
-        continue
+        # if audio_to_video_check is checked
+        if event == "audio_to_video_check":
+            toggle_export_options(window, input_audio_only_or_extract_audio)
+            toggle_attach_image(values["audio_to_video_check"])
 
-    # if ffmpeg is done
-    if event == "ffmpeg_done":
-        window["status"].update(get_status_text(0, True))
-        window["progressbar"].update(0)
-        window["start"].update(disabled=False)
-        if values["ffmpeg_done"] == "error":
-            continue
-        joined_path = os.path.join(values["clip_loc"], values["output_file"])
-        done_message(joined_path)
-        just_clipped = values["output_file"]
-        # break
+        # if audio_only_check is checked
+        if event == "audio_only_check":
+            input_audio_only_or_extract_audio = values["audio_only_check"]
+            toggle_export_options(window, input_audio_only_or_extract_audio)
 
-    # if about button is pressed
-    if event == "about":
-        open_about_window()
+        # if about button is pressed
+        if event == "about":
+            open_about_window()
 
     print(event)
     print(values)
+
+        print(event)
+        print(values)
+
+
+if __name__ == "__main__":
+    check_for_fftools()
+    main_app()

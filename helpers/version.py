05c6a5a02a49516d86283f3b224916820858909b
hako⭐ <forrestlightspeed@gmail.com>
2025-04-19 04:12:16 -0500

    update version

--- helpers/version.py
+++ helpers/version.py
@@ -1,19 +1,10 @@
-CURRENT = "1.2.2"
+CURRENT = "1.2.8"
 CHANGES = """
-(1.2.0) Added support for clipping audio files.
-Refactored data validation and main event loop.
-Added auto resolution/FPS button.
-Added GPU detection and encoder choice for NVENC.
-Make "Done!" message box go away on its own.
+(1.2.8) Small facelift. Fixed some crashes.
 
-(1.1.6) Check for special characters in filename now obeys Windows filename rules.
-Added this About window.
-Refactored settings file save/load.
-Saving is more robust on window close.
-Fixed crash when non-numeric characters typed in FPS box.
-Fixed an error when clipping a video with a resolution not divisible by 2.
-Added error handling for missing binaries.
-Fixed the previous fix for the FPS box. FPS box now only accepts numbers."""
+(1.2.4) Start and end time sliders can now be scrolled with the mouse wheel.
+
+"""
 
 """
 1.0 = first clippy release
@@ -31,6 +22,24 @@ todo: the settings saving and loading is really really bad. very hard coded. ref
 todo: maybe make resolution/fps check adjust the settings to the most ideal for the current file
 1.2.0 = added support for audio files; refactored main loop and data validation; added auto resolution button; refactor ffprobe process
 1.2.1 = add gpu detection and encoder choice, if nvidia gpu is present
+1.2.2 = idk
+1.2.3 = add "remux" option for encoder bc i need mkv's to be mp4's
 
 todo: add automatic updates so i don't have to send a whole file over discord to people every single time
 """
+
+"""old versions
+(1.2.0) Added support for clipping audio files.
+Refactored data validation and main event loop.
+Added auto resolution/FPS button.
+Added GPU detection and encoder choice for NVENC.
+Make "Done!" message box go away on its own.
+
+(1.1.6) Check for special characters in filename now obeys Windows filename rules.
+Added this About window.
+Refactored settings file save/load.
+Saving is more robust on window close.
+Fixed crash when non-numeric characters typed in FPS box.
+Fixed an error when clipping a video with a resolution not divisible by 2.
+Added error handling for missing binaries.
+Fixed the previous fix for the FPS box. FPS box now only accepts numbers."""
